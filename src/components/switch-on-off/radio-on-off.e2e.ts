e8fe66cb77fa2c7f041aa316d0f5dfb5cd41231d
Kyle Ramie <kramie09@gmail.com>
2019-02-12 09:26:21 -0500

    Fix e2e tests for se-button and se-radio-on-off

--- src/components/switch-on-off/radio-on-off.e2e.ts
+++ src/components/switch-on-off/radio-on-off.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('SwitchOnOffComponent', () => {
+describe('RadioOnOffComponent', () => {
   it('renders', async () => {
     const page = await newE2EPage();
 
-    await page.setContent('<se-navbar></se-navbar>');
-    const element = await page.find('se-navbar');
+    await page.setContent('<se-radio-on-off></se-radio-on-off>');
+    const element = await page.find('se-radio-on-off');
     expect(element).toHaveClass('hydrated');
   });
 });
