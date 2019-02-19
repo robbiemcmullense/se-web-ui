d810f402a8ead8ddabba7b9d99f1cf03e2c9302a
Francios Delpech <>
2019-02-19 08:47:54 -0500

    replace full by fill for content

--- src/components.d.ts
+++ src/components.d.ts
@@ -115,9 +115,9 @@ export namespace Components {
 
   interface SeContainer {
     /**
-    * Defines the inner apparance of a container. `widget` Add a small spacing all around the container so all widgets are spaced with the same distance. `full` Default. Take the full space of the container. `centered` center the container so the content does no exceed a max width.
+    * Defines the inner apparance of a container. `widget` Add a small spacing all around the container so all widgets are spaced with the same distance. `fill` Default. Take the full space of the container. `centered` center the container so the content does no exceed a max width.
     */
-    'mode': 'widget' | 'full' | 'centered';
+    'mode': 'widget' | 'fill' | 'centered';
     /**
     * In specific case, it can be necessary to define the container with an absolute position (inside an angular router-container ). Most of the time, the default position will work perfectly with flex box. `relative` Default. Perfect to use with flex content. `absolute` Help in specific cases. Make sure you know that you are doing.
     */
@@ -125,9 +125,9 @@ export namespace Components {
   }
   interface SeContainerAttributes extends StencilHTMLAttributes {
     /**
-    * Defines the inner apparance of a container. `widget` Add a small spacing all around the container so all widgets are spaced with the same distance. `full` Default. Take the full space of the container. `centered` center the container so the content does no exceed a max width.
+    * Defines the inner apparance of a container. `widget` Add a small spacing all around the container so all widgets are spaced with the same distance. `fill` Default. Take the full space of the container. `centered` center the container so the content does no exceed a max width.
     */
-    'mode'?: 'widget' | 'full' | 'centered';
+    'mode'?: 'widget' | 'fill' | 'centered';
     /**
     * In specific case, it can be necessary to define the container with an absolute position (inside an angular router-container ). Most of the time, the default position will work perfectly with flex box. `relative` Default. Perfect to use with flex content. `absolute` Help in specific cases. Make sure you know that you are doing.
     */
