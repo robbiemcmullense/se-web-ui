321bc381debdeef53af58d12fedd8a0d4f54c5c9
Kyle Ramie <kramie09@gmail.com>
2019-02-26 17:31:02 -0500

    add container e2e test

--- src/components/container/container.e2e.ts
+++ src/components/container/container.e2e.ts
@@ -3,9 +3,25 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('ContainerComponent', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-
     await page.setContent('<se-container></se-container>');
+
     const element = await page.find('se-container');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders the "fill-content" and "relative" classes by default', async () => {	
+    const page = await newE2EPage();
+    await page.setContent('<se-container></se-container>');
+    
+    const element = await page.find('se-container');	
+    expect(element).toHaveClasses(['fill-content', 'relative']);	
+  });
+
+  it('renders the "centered" and "absolute-content" classes when these properties are specified in the DOM', async () => {	
+    const page = await newE2EPage();
+    await page.setContent('<se-container mode="centered" position="absolute"></se-container>');
+    
+    const element = await page.find('se-container');	
+    expect(element).toHaveClasses(['centered-content', 'absolute']);
   });
 });
