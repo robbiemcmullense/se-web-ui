f8eb70f8529a0a4368629b02e559746ec1d46c76
Kyle Ramie <kramie09@gmail.com>
2019-02-27 10:30:00 -0500

    add e2e tests for label and link components

--- src/components/label/label.e2e.ts
+++ src/components/label/label.e2e.ts
@@ -3,9 +3,28 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('LabelComponent', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-
     await page.setContent('<se-label></se-label>');
+
     const element = await page.find('se-label');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders with the label inner text equal to the value property', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-label value="Label Value"></se-label>');
+    
+    const element = await page.find('se-label >>> label');
+    const spanElement = await page.find('se-label >>> span');
+    expect(element.innerText).toEqual('Label Value');
+    expect(spanElement).toBeNull();
+  });
+
+  it('renders with an asterisk when required is set to true', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-label value="Label Value" required="true"></se-label>');
+    
+    const element = await page.find('se-label >>> span');
+    expect(element).not.toBeNull();
+    expect(element.innerHTML).toEqual('*');
   });
 });
--- src/components/link/link.e2e.ts
+++ src/components/link/link.e2e.ts
@@ -3,9 +3,33 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('LinkComponent', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-
     await page.setContent('<se-link></se-link>');
+
     const element = await page.find('se-link');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders with text equal to the link property', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-link link="test link"></se-link>');
+    
+    const element = await page.find('se-link >>> a');
+    expect(element.innerText).toEqual('test link');
+  });
+
+  it('renders with an href equal to the url property', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-link url="testURL"></se-link>');
+    
+    const element = await page.find('se-link');
+    expect(element.shadowRoot).toEqualHtml('<a href="testURL" target=""></a>')
+  });
+
+  it('renders with a blank target attribute when the type is set to external', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-link url="testURL" type="external"></se-link>');
+    
+    const element = await page.find('se-link');
+    expect(element.shadowRoot).toEqualHtml('<a class="external" href="testURL" target="_blank"></a>')
   });
 });
