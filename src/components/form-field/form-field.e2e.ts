b2d1df73b91b82c34e072e61629b80e26571a4fc
Kyle Ramie <kramie09@gmail.com>
2019-02-27 10:06:26 -0500

    add header and form-field e2e  tests

--- src/components/form-field/form-field.e2e.ts
+++ src/components/form-field/form-field.e2e.ts
@@ -3,9 +3,20 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('FormFieldComponent', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-
     await page.setContent('<se-form-field></se-form-field>');
+
     const element = await page.find('se-form-field');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders with a label with its value equal to the given label property', async () => {
+    const page = await newE2EPage();
+    const renderedHTML = '<div class="se-form-field">'
+      + '<se-label class="hydrated"></se-label>'
+      + '<slot></slot></div>'
+    await page.setContent('<se-form-field></se-form-field>');
+
+    const element = await page.find('se-form-field');
+    expect(element.shadowRoot).toEqualHtml(renderedHTML);
   });
 });
--- src/components/header/header.e2e.ts
+++ src/components/header/header.e2e.ts
@@ -3,9 +3,37 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('HeaderComponent', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-
     await page.setContent('<se-header></se-header>');
+
     const element = await page.find('se-header');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('sets the application title to both the font-regular and font-lighter elements', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-header app-title="Test Application"></se-header>');
+    
+    const titleFirstName = await page.find('se-header >>> .font-regular');
+    const titleLastName = await page.find('se-header >>> .font-lighter');
+    expect(titleFirstName.innerText).toEqual('Test');
+    expect(titleLastName.innerHTML).toEqual('&nbsp;Application');
+  });
+
+  it('renders an se-ecostruxure component by default', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-header app-title="Test Application"></se-header>');
+    
+    const iconComponent = await page.find('se-header >>> se-icon-ecostruxure');
+    expect(iconComponent).toBeTruthy();
+  });
+
+  it('does not renders an se-ecostruxure component when the domain property is changed', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<se-header app-title="Test Application" domain="testDomain"></se-header>');
+    
+    const iconComponent = await page.find('se-header >>> se-icon-ecostruxure');
+    const domainElement = await page.find('se-header >>> .header-title-type');
+    expect(iconComponent).toBeNull();
+    expect(domainElement.innerText).toEqual('testDomain');
   });
 });
