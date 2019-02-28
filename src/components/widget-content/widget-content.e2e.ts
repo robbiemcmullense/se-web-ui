d872bef5638480ee32eeaf895aa49aa0acd4b44d
Kyle Ramie <kramie09@gmail.com>
2019-02-28 12:06:29 -0500

    add widget e2e tests, remove duplicate code in package.json

--- src/components/widget-content/widget-content.e2e.ts
+++ src/components/widget-content/widget-content.e2e.ts
@@ -1,11 +1,24 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('WidgetContentComponent', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
+  let page, element;
 
+  beforeEach(async() => {
+    page = await newE2EPage();
     await page.setContent('<se-widget-content></se-widget-content>');
-    const element = await page.find('se-widget-content');
+    element = await page.find('se-widget-content');
+  });
+
+  it('renders', async() => {
+    expect(element).toBeTruthy();
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders with the mode class whenever the mode is set to fill', async() => {
+    await page.$eval('se-widget-content', (elm: any) => {
+      elm.mode = 'fill';
+    });
+    await page.waitForChanges();
+    expect(element).toHaveClass('full-content');
   });
 });
--- src/components/widget/widget.e2e.ts
+++ src/components/widget/widget.e2e.ts
@@ -1,11 +1,28 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('WidgetComponent', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
+  let page, element;
 
+  beforeEach(async() => {
+    page = await newE2EPage();
     await page.setContent('<se-widget></se-widget>');
-    const element = await page.find('se-widget');
+    element = await page.find('se-widget');
+  });
+
+  it('renders', async() => {
+    expect(element).toBeTruthy();
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders with a flex class by default with no specified height or width', async() => {
+    expect(element).toHaveClass('flex');
+  });
+
+  it('renders without a flex class because there is a specified width', async() => {
+    await page.$eval('se-widget', (elm: any) => {
+      elm.width = '300px';
+    });
+    await page.waitForChanges();
+    expect(element).not.toHaveClass('flex');
   });
 });
