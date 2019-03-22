be5b1905614eaecdcd8bf9747126b3f84aae55aa
Kyle Ramie <kramie09@gmail.com>
2019-03-22 13:54:37 -0400

    make switch component compatible with edge

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,28 +1,44 @@
-import { Component, Event, EventEmitter, State } from '@stencil/core';
+import { Component, Element, Event, EventEmitter, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'se-switch',
   styleUrl: 'switch.scss',
   shadow: true
 })
-
 export class SwitchComponent {
+	/**
+   * Optional property that defines if the switch is disabled.  Set to `false` by default.
+   */
+	@Prop() disabled: boolean = false;
 	
 	@State() selected: boolean;
 	/**
 	 * Send the state of the switch (true/false) to the parent component when it is toggled.
 	 */
 	@Event() change: EventEmitter;
-	
 
+	@Element() el: HTMLElement;
+	
 	emitEvent() {
-		this.selected = !this.selected;
-		this.change.emit({selected: this.selected});
+		if (!this.disabled) {
+			this.selected = !this.selected;
+			if (this.selected) {
+				this.el.classList.add('selected');
+			} else {
+				setTimeout(() => {
+					this.el.classList.remove('selected');
+				}, 200)
+			}
+			this.change.emit({selected: this.selected});
+		}
 	}
 
 	render() {
 		return (
-			<input class="se-switch" type="checkbox" onClick={() => this.emitEvent()} />
+			<label class="switch-container" data-disabled={this.disabled}>
+				<input type="checkbox" disabled={this.disabled}/>
+				<span class="switch-button" onClick={() => this.emitEvent()} />
+			</label>
 		);
 	}
 }
