2ddef71698d40f42ddfedfb0a35985fba5f9c1ff
Francios Delpech <>
2019-05-03 14:24:07 -0400

    clean up unit test and radio group

--- src-angular/projects/frameworks/src/lib/dialog/dialog.service.ts
+++ src-angular/projects/frameworks/src/lib/dialog/dialog.service.ts
@@ -1,55 +1,73 @@
-import { Injectable,Injector,
+import {
+  Injectable,
+  Injector,
   ComponentFactoryResolver,
   EmbeddedViewRef,
   ComponentRef,
-  ApplicationRef,Type  } from '@angular/core';
+  ApplicationRef,
+  Type
+} from '@angular/core';
 
-import { DialogComponent,DialogModalComponent} from "../dialog/dialog.component";
-import { DialogConfig} from '../dialog/dialog-config';
-import {ComponentInjector} from '../shared/component-injector';
+import {
+  DialogComponent,
+  DialogModalComponent
+} from '../dialog/dialog.component';
+import { DialogConfig } from '../dialog/dialog-config';
+import { ComponentInjector } from '../shared/component-injector';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class DialogService {
   dialogRef: ComponentRef<DialogComponent>;
   modalRef: ComponentRef<DialogModalComponent>;
 
   public componentRef: ComponentRef<any>;
-  constructor(private componentFactoryResolver: ComponentFactoryResolver, private appRef: ApplicationRef,private injector: Injector) { }
-    
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector
+  ) {}
+
   /**
    * @name createDialogComponent
    * @param componentType
    * @param config
    * @description method to create dialog component dynamically
    */
-  public createDialogComponent(componentType: Type<any>,config?:DialogConfig){
-     const map = new WeakMap();
-     map.set(DialogConfig, config?config:{});      
-    this.componentRef = this.componentFactoryResolver.resolveComponentFactory(componentType)
-    .create(new ComponentInjector(this.injector,map));
+  public createDialogComponent(
+    componentType: Type<any>,
+    config?: DialogConfig
+  ) {
+    const map = new WeakMap();
+    map.set(DialogConfig, config ? config : {});
+    this.componentRef = this.componentFactoryResolver
+      .resolveComponentFactory(componentType)
+      .create(new ComponentInjector(this.injector, map));
     this.appRef.attachView(this.componentRef.hostView);
-    const domElem = (this.componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement; 
+    const domElem = (this.componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
     document.body.appendChild(domElem);
     return this.componentRef;
   }
-  
+
   /**
    * @name alert
    * @param config
    * @description method to open alert dialog
-   */ 
+   */
 
-  public alert(config:DialogConfig){
-    let alertDialogRef = this.createDialogComponent(DialogComponent,config);
-    alertDialogRef.instance.type ='alert';
-    alertDialogRef.instance.afterClosed.subscribe((data:any)=>{
-      this.close();
-    },(err: any) => {
-      this.close();
-    });
+  public alert(config: DialogConfig) {
+    const alertDialogRef = this.createDialogComponent(DialogComponent, config);
+    alertDialogRef.instance.type = 'alert';
+    alertDialogRef.instance.afterClosed.subscribe(
+      (data: any) => {
+        this.close();
+      },
+      (err: any) => {
+        this.close();
+      }
+    );
     return alertDialogRef;
   }
   /**
@@ -57,14 +75,17 @@ export class DialogService {
    * @param config
    * @description method to open confirm dialog
    */
-  public confirm(config:DialogConfig){
-    let confirmDialogRef = this.createDialogComponent(DialogComponent,config);
-    confirmDialogRef.instance.type ='confirm';
-    confirmDialogRef.instance.afterClosed.subscribe(()=>{
-      this.close();
-    },(err: any) => {
-      this.close();
-    });
+  public confirm(config: DialogConfig) {
+    let confirmDialogRef = this.createDialogComponent(DialogComponent, config);
+    confirmDialogRef.instance.type = 'confirm';
+    confirmDialogRef.instance.afterClosed.subscribe(
+      () => {
+        this.close();
+      },
+      (err: any) => {
+        this.close();
+      }
+    );
     return confirmDialogRef;
   }
   /**
@@ -72,16 +93,22 @@ export class DialogService {
    * @param componentType
    * @param config
    * @description method to open modal dialog
-   */ 
- 
-  public modal(componentType: Type<any>,config?:DialogConfig){
-    let modalDialogRef = this.createDialogComponent(DialogModalComponent,config);
+   */
+
+  public modal(componentType: Type<any>, config?: DialogConfig) {
+    let modalDialogRef = this.createDialogComponent(
+      DialogModalComponent,
+      config
+    );
     this.componentRef.instance.childComponentType = componentType;
-    modalDialogRef.instance.afterClosed.subscribe(()=>{
-      this.close();
-    },(err: any) => {
-      this.close();
-    });
+    modalDialogRef.instance.afterClosed.subscribe(
+      () => {
+        this.close();
+      },
+      (err: any) => {
+        this.close();
+      }
+    );
     return modalDialogRef;
   }
 
@@ -89,16 +116,16 @@ export class DialogService {
    * @name close
    * @description method to close the dialog by setting close property to true
    */
-  public close(){
-      this.appRef.detachView(this.componentRef.hostView);
-      this.componentRef.destroy();
+  public close() {
+    this.appRef.detachView(this.componentRef.hostView);
+    this.componentRef.destroy();
   }
   /**
    * @name cancel
    * @description method to close the dialog by setting close property to true
    */
-  public cancel(){
-      this.appRef.detachView(this.componentRef.hostView);
-      this.componentRef.destroy();
+  public cancel() {
+    this.appRef.detachView(this.componentRef.hostView);
+    this.componentRef.destroy();
   }
 }
