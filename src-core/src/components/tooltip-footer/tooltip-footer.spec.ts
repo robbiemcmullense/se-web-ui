acaa4925c64ef2483aafee3392f2072609a0ff52
Kyle Ramie <sesa500245@se.com>
2019-08-13 16:12:32 -0400

    get average branch coverage over 50%

--- src-core/src/components/tooltip-footer/tooltip-footer.spec.ts
+++ src-core/src/components/tooltip-footer/tooltip-footer.spec.ts
@@ -1,4 +1,5 @@
 import { TooltipFooterComponent } from './tooltip-footer';
+import { newSpecPage } from '@stencil/core/testing';
 
 describe('TooltipFooterComponent', () => {
   let tooltipFooter;
@@ -11,4 +12,20 @@ describe('TooltipFooterComponent', () => {
     expect(tooltipFooter).toBeTruthy();
   });
 
+  it('should render', async() => {
+    const page = await newSpecPage({
+			components: [TooltipFooterComponent],
+			html: `<se-tooltip-footer></se-tooltip-footer>`,
+		});
+		expect(page.root).toEqualHtml(`
+			<se-tooltip-footer>
+				<mock:shadow-root>
+					<div class="flex">
+						<slot name="start"></slot>
+          </div>
+          <slot></slot>
+				</mock:shadow-root>
+			</se-tooltip-footer>
+		`);
+  });
 });
