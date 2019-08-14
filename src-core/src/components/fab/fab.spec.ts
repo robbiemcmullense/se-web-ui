6d4fb3414a2bf68695ac2adab4753fcf5f22e52a
Kyle Ramie <sesa500245@se.com>
2019-08-14 10:12:15 -0400

    increase code coverage

--- src-core/src/components/fab/fab.spec.ts
+++ src-core/src/components/fab/fab.spec.ts
@@ -1,4 +1,5 @@
 import { FabComponent } from './fab';
+import { newSpecPage } from '@stencil/core/testing';
 
 describe('FabComponent', () => {
   let fab;
@@ -30,5 +31,40 @@ describe('FabComponent', () => {
   it('should change the toggle icon property when the toggle action method is called', () => {
     fab.toggleAction();
     expect(fab.toggleIcon).toBeFalsy();
+  });
+
+  it('should render', async() => {
+    const page = await newSpecPage({
+			components: [FabComponent],
+			html: `<se-fab></se-fab>`,
+		});
+		expect(page.root).toEqualHtml(`
+			<se-fab>
+				<mock:shadow-root>
+          <div class="pos-bottom se-fab">
+          <se-button color="primary" option="fab"></se-button>
+            <div class="mini-action-button">
+              <slot></slot>
+            </div>
+          </div>
+				</mock:shadow-root>
+			</se-fab>
+		`);
+  });
+
+  it('should render with the arrow4_top icon when the option is set to backtotop', async() => {
+    const page = await newSpecPage({
+			components: [FabComponent],
+			html: `<se-fab option="backtotop"></se-fab>`,
+		});
+		expect(page.root).toEqualHtml(`
+			<se-fab option="backtotop">
+				<mock:shadow-root>
+          <div class="pos-bottom se-fab">
+          <se-button color="primary" class="backtotop" option="fab" icon="arrow4_top"></se-button>
+          </div>
+				</mock:shadow-root>
+			</se-fab>
+		`);
   });
 });
