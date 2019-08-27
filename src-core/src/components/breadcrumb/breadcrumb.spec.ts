bceaa77c31967854ab4ca084dae281d285dc157a
Kyle Ramie <sesa500245@se.com>
2019-08-27 14:10:18 -0400

    increase coverage for sidemenu, breadcrumb, button

--- src-core/src/components/breadcrumb/breadcrumb.spec.ts
+++ src-core/src/components/breadcrumb/breadcrumb.spec.ts
@@ -17,6 +17,10 @@ describe('BreadcrumbComponent', () => {
 		expect(breadcrumb.items.length).toEqual(0);
 	});
 
+	it('should have a defined element property', () => {
+		expect(breadcrumb.el).toBeDefined();
+	});
+
 	it('should render', async() => {
 		const page = await newSpecPage({
 			components: [BreadcrumbComponent],
@@ -35,9 +39,50 @@ describe('BreadcrumbComponent', () => {
 		`);
 	});
 
-	it('should call the updateLastItem function when the component loads', async() => {
+	it('should render with two breadcrumb items', async() => {
+		const page = await newSpecPage({
+			components: [BreadcrumbComponent],
+			html: `<se-breadcrumb><se-breadcrumb-item>First</se-breadcrumb-item><se-breadcrumb-item>Second</se-breadcrumb-item></se-breadcrumb>`,
+		});
+		expect(page.root).toEqualHtml(`
+			<se-breadcrumb>
+				<mock:shadow-root>
+					<nav aria-label="breadcrumb">
+						<ol>
+							<slot></slot>
+						</ol>
+					</nav>
+				</mock:shadow-root>
+				<se-breadcrumb-item>First</se-breadcrumb-item>
+				<se-breadcrumb-item>Second</se-breadcrumb-item>
+			</se-breadcrumb>
+		`);
+	});
+
+	it('should call the updateLastItem function when componentWillLoad is executed', () => {
 		const eventSpy = jest.spyOn(breadcrumb, 'updateLastItem');
 		breadcrumb.componentWillLoad();
+		expect(eventSpy).toHaveBeenCalled();
+	});
+
+	it('should call the watchItemList function when componentWillLoad is executed', () => {
+		const eventSpy = jest.spyOn(breadcrumb, 'watchItemList');
+		breadcrumb.componentWillLoad();
+		expect(eventSpy).toHaveBeenCalled();
+	});
+
+	it('should assign a mutation observer for Edge browsers when componentDidLoad is called', () => {
+		breadcrumb.observer = {observe: jest.fn()};
+		const eventSpy = jest.spyOn(breadcrumb.observer, 'observe');
+		Object.defineProperty(window.navigator, 'userAgent', {value: 'Edge'});
+		breadcrumb.componentDidLoad();
+		expect(eventSpy).toHaveBeenCalled();
+	});
+
+	it('should disconnect the mutation observer when componentDidUnload is called', () => {
+		breadcrumb.observer = {disconnect: jest.fn()};
+		const eventSpy = jest.spyOn(breadcrumb.observer, 'disconnect');
+		breadcrumb.componentDidUnload();
 		expect(eventSpy).toHaveBeenCalled();
 	});
 });
