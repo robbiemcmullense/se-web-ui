99734fb11d9b3dc53fe43a411e2b49d0d4ee6501
Kyle Ramie <sesa500245@se.com>
2019-09-04 15:01:24 -0400

    fix angular build for Edge, IE11 browsers

--- demo-angular/src_web/main.ts
+++ demo-angular/src_web/main.ts
@@ -8,11 +8,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { applyPolyfills, defineCustomElements } from '@se/web-ui/loader';
+import { environment } from '@env/environment';
+import { defineCustomElements } from '@se/web-ui/loader';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err: any) => console.log(err));
-  applyPolyfills().then(() => {
-    defineCustomElements(window)
-  })
+defineCustomElements(window);
