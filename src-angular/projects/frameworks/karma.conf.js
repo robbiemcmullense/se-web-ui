41a5dfaa829261ba5edda15c94a2314641985cb1
Francois Delpech de Frayssinet <Francois.Delpech@schneider-electric.com>
2019-09-04 18:41:35 -0400

    Update karma.conf.js

--- src-angular/projects/frameworks/karma.conf.js
+++ src-angular/projects/frameworks/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless'], // To run on Jenkins
     singleRun: false
   });
 };
