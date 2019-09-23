c78dacceb987b225728faad973fc97d46a435d61
Guillaume Brialon <SESA536352@se.com>
2019-09-23 11:40:29 +0200

    Build-related light fixes

--- src-core/src/components/checkbox/checkbox.tsx
+++ src-core/src/components/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Event, EventEmitter, h, Method, Prop, State } from "@stencil/core";
+import { Component, Element, Event, EventEmitter, h, Method, Prop } from "@stencil/core";
 
 @Component({
   tag: "se-checkbox",
