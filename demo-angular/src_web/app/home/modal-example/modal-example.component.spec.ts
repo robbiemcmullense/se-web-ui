9a281cd980c810eae3325e94d70891b59971785c
Kyle Ramie <sesa500245@se.com>
2019-10-01 10:38:57 -0400

    fix failing unit test

--- demo-angular/src_web/app/home/modal-example/modal-example.component.spec.ts
+++ demo-angular/src_web/app/home/modal-example/modal-example.component.spec.ts
@@ -1,4 +1,4 @@
-import { SeWebModule } from '@se/web-ui-angular';
+import { SeWebModule, MODAL_DATA } from '@se/web-ui-angular';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { ModalExampleComponent } from './modal-example.component';
@@ -6,11 +6,16 @@ import { ModalExampleComponent } from './modal-example.component';
 describe('ModalExampleComponent', () => {
   let component: ModalExampleComponent;
   let fixture: ComponentFixture<ModalExampleComponent>;
+  let mockData = {
+    username: 'my user',
+    password: '123456'
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SeWebModule, FormsModule],
-      declarations: [ModalExampleComponent]
+      declarations: [ModalExampleComponent],
+      providers: [{provide: MODAL_DATA, useValue: mockData}]
     })
     .compileComponents();
   }));
