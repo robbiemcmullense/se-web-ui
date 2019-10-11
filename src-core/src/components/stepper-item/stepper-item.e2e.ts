d3d3668eb637259381452ecbfb5f3fd93e00c44c
Kyle Ramie <sesa500245@se.com>
2019-10-11 13:32:58 -0400

    add tests for stepper item

--- src-core/src/components/stepper-item/stepper-item.e2e.ts
+++ src-core/src/components/stepper-item/stepper-item.e2e.ts
@@ -1,16 +1,39 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('StepperItemComponent', () => {
-  let page, element;
+  let page, hostElement, element;
 
   beforeEach(async() => {
     page = await newE2EPage();
     await page.setContent('<se-stepper-item></se-stepper-item>');
+    hostElement = await page.find('se-stepper-item');
     element = await page.find('se-stepper-item >>> div');
   });
 
-  it('renders', async() => {
+  it('renders, and the root element has a stepper-item-wrapper class', async() => {
+    expect(hostElement).toBeTruthy();
+    expect(hostElement).toHaveClass('hydrated');
     expect(element).toBeTruthy();
     expect(element).toHaveClass('stepper-item-wrapper');
+  });
+
+  it('renders a li element with the class stepper-item', async() => {
+    let listItem = await page.find('se-stepper-item >>> li');
+    expect(listItem).toHaveClass('stepper-item');
+  });
+
+  it('renders se-icon and se-divider elements', async() => {
+    let iconElm = await page.find('se-stepper-item >>> se-icon');
+    let divider = await page.find('se-stepper-item >>> se-divider');
+    expect(iconElm).toBeTruthy();
+    expect(divider).toBeTruthy();
+  });
+
+  it('does not render a se-divider element when isLast is true', async() => {
+    hostElement.setProperty('isLast', true);
+    await page.waitForChanges();
+
+    let divider = await page.find('se-stepper-item >>> se-divider');
+    expect(divider).not.toBeTruthy();
   });
 });
--- src-core/src/components/stepper-item/stepper-item.spec.ts
+++ src-core/src/components/stepper-item/stepper-item.spec.ts
@@ -1,4 +1,5 @@
 import { StepperItemComponent } from './stepper-item';
+import { newSpecPage } from '@stencil/core/testing';
 
 describe('StepperItemComponent', () => {
 	let stepperItem;
@@ -13,5 +14,30 @@ describe('StepperItemComponent', () => {
 
   it('should not be the last stepper item by defualt', () => {
     expect(stepperItem.isLast).toBeFalsy();
-  });
+	});
+	
+	it('should render with a list item with the stepper-item class', async() => {
+		const page = await newSpecPage({
+			components: [StepperItemComponent],
+			html: `<se-stepper-item></se-stepper-item>`,
+		});
+		expect(page.root.shadowRoot.querySelector('li.stepper-item')).toBeTruthy();
+	});
+
+	it('should render with se-icon and se-divider elements', async() => {
+		const page = await newSpecPage({
+			components: [StepperItemComponent],
+			html: `<se-stepper-item></se-stepper-item>`,
+		});
+		expect(page.root.shadowRoot.querySelector('se-icon')).toBeTruthy();
+		expect(page.root.shadowRoot.querySelector('se-divider')).toBeTruthy();
+	});
+
+	it('should not render the se-divider when is-last is true', async() => {
+		const page = await newSpecPage({
+			components: [StepperItemComponent],
+			html: `<se-stepper-item is-last="true"></se-stepper-item>`,
+		});
+		expect(page.root.shadowRoot.querySelector('se-divider')).not.toBeTruthy();
+	});
 });
