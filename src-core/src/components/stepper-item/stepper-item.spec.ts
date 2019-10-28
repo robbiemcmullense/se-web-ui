dd631a8599ffa5e526978112923741c981e11f87
Kyle Ramie <sesa500245@se.com>
2019-10-28 10:02:44 -0400

    add unit tests for stepper, stepper-item

--- src-core/src/components/stepper-item/stepper-item.spec.ts
+++ src-core/src/components/stepper-item/stepper-item.spec.ts
@@ -10,7 +10,11 @@ describe('StepperItemComponent', () => {
 
 	it('should build', () => {
 		expect(stepperItem).toBeTruthy();
-  });
+	});
+	
+	it('should not have a defined label by default', () => {
+		expect(stepperItem.label).toBeUndefined();
+	});
 
   it('should not be the last stepper item by defualt', () => {
     expect(stepperItem.isLast).toBeFalsy();
@@ -33,11 +37,25 @@ describe('StepperItemComponent', () => {
 		expect(page.root.shadowRoot.querySelector('se-divider')).toBeTruthy();
 	});
 
+	it('should render the label property as the inner text of a list item element', async() => {
+		const page = await newSpecPage({
+			components: [StepperItemComponent],
+			html: `<se-stepper-item label="My Label"></se-stepper-item>`,
+		});
+		expect(page.root.shadowRoot.querySelector('.stepper-item').innerHTML).toEqual('My Label');
+	});
+
 	it('should not render the se-divider when is-last is true', async() => {
 		const page = await newSpecPage({
 			components: [StepperItemComponent],
 			html: `<se-stepper-item is-last="true"></se-stepper-item>`,
 		});
 		expect(page.root.shadowRoot.querySelector('se-divider')).not.toBeTruthy();
+	});
+
+	it('should emit the didClick event when clicking on a stepper item', () => {
+		const eventSpy = jest.spyOn(stepperItem.didClick, 'emit');
+		stepperItem.emitEvent(); // user clicks on stepper item
+		expect(eventSpy).toHaveBeenCalled();
 	});
 });
--- src-core/src/components/stepper/stepper.spec.ts
+++ src-core/src/components/stepper/stepper.spec.ts
@@ -14,7 +14,31 @@ describe('StepperComponent', () => {
 
   it('should have a primary color by default', () => {
     expect(stepper.color).toBe('primary');
-  });
+	});
+
+	it('should not be linear by default', () => {
+		expect(stepper.linear).toBeFalsy();
+	});
+	
+	it('should add to the items array when a stepper item is added, and be active and last by default', () => {
+		let node = document.createElement('se-stepper-item');
+    stepper.el.appendChild(node);
+		stepper.componentDidLoad();
+		expect(stepper.items.length).toEqual(1);
+		expect(stepper.items[0].isLast).toBeTruthy();
+		expect(stepper.items[0]).toHaveClass('active');
+	});
+
+	it('should not set the isLast property to the first stepper item when there are two stepper items', () => {
+		let nodeOne = document.createElement('se-stepper-item');
+		stepper.el.appendChild(nodeOne);
+		let nodeTwo = document.createElement('se-stepper-item');
+    stepper.el.appendChild(nodeTwo);
+		stepper.componentDidLoad();
+		expect(stepper.items.length).toEqual(2);
+		expect(stepper.items[0].isLast).toBeFalsy();
+		expect(stepper.items[1]).not.toHaveClass('active');
+	});
 	
 	it('should render', async() => {
 		const page = await newSpecPage({
