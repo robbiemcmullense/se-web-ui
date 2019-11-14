67eff108fd8ecab1cb132432e65befd2cb034957
Cathy O’Brien <Catherine.OBrien@non.se.com>
2019-11-14 13:22:21 -0500

    Tabbar content initial

--- src-core/src/components/tabbar/tabbar.tsx
+++ src-core/src/components/tabbar/tabbar.tsx
@@ -7,31 +7,35 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class TabbarComponent {
   /**
-   * Indicates the color of your tab bar.
+   * Defines the function of the tabbar.
+   * Default `nav` creates a tabbar that functions as a nav-bar.
+   * `content` creates a tabbar that functions as a content section tabbar.
+   */
+  @Prop() option: "nav" | "content"  = "nav";
+  /**
+   * Indicates the color of your tabbar.
    * Default setting is `primary`, rendering a green background.
    * The `alternative` setting renders a white background.
    */
   @Prop() color: 'primary' | 'alternative' = 'primary';
   /**
    * Defines the inner appearance of a tabbar.
-   * `fill` is the default option, taking the full space of the tabbar.
+   * `fill` is the default display, taking the full space of the tabbar.
    * `centered` centers the tabbar so the content does not exceed a maximum width (1070px by default).
    */
-  @Prop() option: "fill" | "centered"  = "fill";
+  @Prop() display: "fill" | "centered"  = "fill";
 
   render() {
     return (
-      <div class={`tab-${this.color}`}>
-        <div class={["d-flex-main", `opt-${this.option}`].join(' ')}>
-          <div class="nav-left-wrapper centered shadowed">
+      <div class={[`tab-${this.color}`, `opt-${this.option}`].join(' ')}>
+        <div class={["d-flex-main", `dis-${this.display}`, `opt-${this.option}`].join(' ')}>
+          <div class={["nav-left-wrapper", "centered", `${this.option === "nav" ? "shadowed" : ''}`].join(' ')}>
             <slot name="start" />
           </div>
-          <div class="fill-space nav-center-wrapper shadowed">
+          <div class={["fill-space", "nav-center-wrapper", `${this.option === "nav" ? "shadowed" : ''}`].join(' ')}>
             <slot />
           </div>
-          <div class="centered shadowed">
-            <slot name="end" />
-          </div>
+          {this.option === "nav" ? <div class="centered shadowed"><slot name="end" /></div> : ''}
         </div>
       </div>
     );
