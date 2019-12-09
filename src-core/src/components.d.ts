053f610a389aee258918cc671cdb33f7e27a0d06
Cathy O’Brien <Catherine.OBrien@non.se.com>
2019-12-09 12:17:27 -0500

    Tab bar style updates (#264)

--- src-core/src/components.d.ts
+++ src-core/src/components.d.ts
@@ -798,10 +798,6 @@ export namespace Components {
     */
     'color': 'primary' | 'alternative';
     /**
-    * Defines the inner appearance of a tabbar. `fill` is the default display, taking the full space of the tabbar. `centered` centers the tabbar so the content does not exceed a maximum width (1070px by default).
-    */
-    'display': "fill" | "centered";
-    /**
     * Defines the function of the tabbar. Default `nav` creates a tab bar that functions as a nav-bar. `content` creates a ta bbar that functions as a content section tab bar.
     */
     'option': "nav" | "content";
@@ -2031,10 +2027,6 @@ declare namespace LocalJSX {
     * Indicates the color of your tab bar. Default setting is `primary`, rendering a green background for nav-bars and ultra-light-grey-1 for content. The `alternative` setting renders a white background.
     */
     'color'?: 'primary' | 'alternative';
-    /**
-    * Defines the inner appearance of a tabbar. `fill` is the default display, taking the full space of the tabbar. `centered` centers the tabbar so the content does not exceed a maximum width (1070px by default).
-    */
-    'display'?: "fill" | "centered";
     /**
     * Defines the function of the tabbar. Default `nav` creates a tab bar that functions as a nav-bar. `content` creates a ta bbar that functions as a content section tab bar.
     */
