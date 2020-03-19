25fc2a0b30433886639a4828130cd7abbe3e7243
Francios Delpech <>
2020-03-19 18:33:27 -0400

    update version

--- libs/angular/jest.config.js
+++ libs/angular/jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/myangularlib',
   snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
+    'jest-preset-angular/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
   ]
 };
