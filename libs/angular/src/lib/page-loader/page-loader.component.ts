aeb692372d472347e814be130aded3e68c4c7071
Julien.1 Minella <julien.1-minella@non.se.com>
2021-04-28 14:18:31 +0200

    angular page loader config (#478)
    * added PageLoaderConfig entry to Angular's PageLoaderService.show
    
    - this allows the user to set the different options on the se-loading embedded inside the component
    - fixed alignment issue with non-dialog loader message
    
    * added loader message option to the PageLoaderConfig
    
    Co-authored-by: Francois Delpech de Frayssinet <Francois.Delpech@schneider-electric.com>

--- libs/angular/src/lib/page-loader/page-loader.component.ts
+++ libs/angular/src/lib/page-loader/page-loader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageLoaderConfig } from './page-loader-config';
 
 @Component({
   selector: 'se-page-loader',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageLoaderComponent implements OnInit {
 
-  constructor() {}
+  constructor(public config: PageLoaderConfig) {}
 
   ngOnInit() {}
 
--- libs/angular/src/lib/page-loader/page-loader.service.ts
+++ libs/angular/src/lib/page-loader/page-loader.service.ts
@@ -1,21 +1,46 @@
-import { Injectable, ComponentFactoryResolver, ApplicationRef, Injector, ComponentRef, EmbeddedViewRef } from '@angular/core';
+import {
+  Injectable,
+  ComponentFactoryResolver,
+  ApplicationRef,
+  Injector,
+  ComponentRef,
+  EmbeddedViewRef,
+} from '@angular/core';
 import { PageLoaderComponent } from './page-loader.component';
 import { ComponentInjector } from '../shared/component-injector';
+import { PageLoaderConfig } from './page-loader-config';
 
 @Injectable()
 export class PageLoaderService {
+  defaultConfig: PageLoaderConfig = {
+    color: 'primary',
+    option: 'standard',
+    fullPage: true,
+    overlay: true,
+    message: '',
+  };
 
   public pageLoaderComponentRef: ComponentRef<PageLoaderComponent>;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver,
-              private applicationRef: ApplicationRef,
-              private injector: Injector) {}
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private applicationRef: ApplicationRef,
+    private injector: Injector
+  ) {}
 
-  public show() {
-    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(PageLoaderComponent);
-    const componentRef = componentFactory.create(new ComponentInjector(this.injector, new WeakMap()));
+  public show(config: PageLoaderConfig = {}) {
+    const userConf: PageLoaderConfig = { ...this.defaultConfig, ...config };
+    const map = new WeakMap<any, any>([[PageLoaderConfig, userConf]]);
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+      PageLoaderComponent
+    );
+    const componentRef = componentFactory.create(
+      new ComponentInjector(this.injector, map)
+    );
     this.applicationRef.attachView(componentRef.hostView);
-    const domElement = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+    const domElement = (componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
     document.body.appendChild(domElement);
     this.pageLoaderComponentRef = componentRef;
   }
@@ -24,5 +49,4 @@ export class PageLoaderService {
     this.applicationRef.detachView(this.pageLoaderComponentRef.hostView);
     this.pageLoaderComponentRef.destroy();
   }
-
 }
